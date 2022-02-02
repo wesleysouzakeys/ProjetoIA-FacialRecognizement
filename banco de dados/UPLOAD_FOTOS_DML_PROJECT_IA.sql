ddb92c3c9043e2313b2211402b15dd3a89695e80
Vitor Marchitiello Labadessa <43055330811@info.scs>
2022-02-02 10:59:54 -0300

    DML

---
+++ banco de dados/UPLOAD_FOTOS_DML_PROJECT_IA.sql
@@ -0,0 +1,6 @@
+USE PROJECT_IA
+GO
+
+INSERT INTO CADCLIENTE(nome, email, senha)
+VALUES ('Gerson Ramalio', 'gerson.ramalio@gmail.com','gersinho') , ('Joel Jimenes','j.jimenes@gmail.com', 'jjjmm')
+GO
